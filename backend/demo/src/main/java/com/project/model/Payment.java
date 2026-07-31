a0b3a718843273062cc111048101a007694d00ad
vini-arima <viniciuscarimateia@gmail.com>
2026-07-31 12:30:17 -0300

    backend - add Payment.java - model

--- backend/demo/src/main/java/com/project/model/Payment.java
+++ backend/demo/src/main/java/com/project/model/Payment.java
@@ -1,5 +1,62 @@
 package com.project.model;
 
 public class Payment {
+
+    private int id ;
+    private double amount ;
+    private PaymentStatus status ;
+    private PaymentMethod method ;
+    private Customer customer ;  
+
+    public Payment(){
+
+    }
+
+    public Payment(int id, double amount, PaymentStatus status, PaymentMethod method, Customer customer) {
+        this.id = id;
+        this.amount = amount;
+        this.status = status;
+        this.method = method;
+        this.customer = customer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public PaymentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PaymentStatus status) {
+        this.status = status;
+    }
+
+    public PaymentMethod getMethod() {
+        return method;
+    }
+
+    public void setMethod(PaymentMethod method) {
+        this.method = method;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    
     
 }

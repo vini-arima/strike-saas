0b1c6a8a79746dccb825f8b7d4d4268c74be3a09
vini-arima <viniciuscarimateia@gmail.com>
2026-08-08 18:13:01 -0300

    backend - repository - CustomerRespository - findById

--- backend/demo/src/main/java/com/project/repository/CustomerRepository.java
+++ backend/demo/src/main/java/com/project/repository/CustomerRepository.java
@@ -2,11 +2,13 @@ package com.project.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+
 import com.project.model.Customer;
 import java.util.Optional;
 
-
 public interface CustomerRepository extends CrudRepository<Customer , Long> {
     Optional <Customer> findByEmail(String email);
-    
+    Optional <Customer> findById(Integer id);
+   
+
 } 

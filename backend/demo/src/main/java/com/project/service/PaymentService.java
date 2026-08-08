bd4e30f711aed93d2f9e1a9a628b0b8c2794ea10
vini-arima <viniciuscarimateia@gmail.com>
2026-08-08 13:37:35 -0300

    backend - service - PaymentService(create-payment)

--- backend/demo/src/main/java/com/project/service/PaymentService.java
+++ backend/demo/src/main/java/com/project/service/PaymentService.java
@@ -1,5 +1,30 @@
 package com.project.service;
 
+import com.project.exception.PayementNotFound;
+import com.project.model.Payment;
+import com.project.repository.PaymentRepository;
+
 public class PaymentService {
+    // injeção dependencia
+    private final PaymentRepository repository;
+
+    public PaymentService(PaymentRepository repository) {
+        this.repository = repository;
+    }
     
+    public Payment createPayment(Payment payment){
+        try {
+            
+        } catch (PayementNotFound e) {
+                
+        }
+
+        repository.save(payment) ;
+        return payment ;
+    }
+
+    
+
+
+
 }

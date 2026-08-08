4707ef3dedd090dcb6ca80813290e1011640b776
vini-arima <viniciuscarimateia@gmail.com>
2026-08-08 17:44:51 -0300

    backend - service - PaymentService - findAll and findById

--- backend/demo/src/main/java/com/project/service/PaymentService.java
+++ backend/demo/src/main/java/com/project/service/PaymentService.java
@@ -1,9 +1,16 @@
 package com.project.service;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.project.exception.CustomerNotFound;
 import com.project.exception.PayementNotFound;
+import com.project.model.Customer;
 import com.project.model.Payment;
 import com.project.repository.PaymentRepository;
 
+@Service
 public class PaymentService {
     // injeção dependencia
     private final PaymentRepository repository;
@@ -21,6 +28,16 @@ public class PaymentService {
 
         repository.save(payment) ;
         return payment ;
+    }
+
+    public Iterable<Payment> findAllPayment(){
+        return repository.findAll() ;
+    } 
+
+    public Payment findPaymentById(Integer id){
+        Optional<Payment> payment = repository.findById(id) ;
+
+        return payment.orElseThrow(CustomerNotFound::new) ;
     }
 
     
